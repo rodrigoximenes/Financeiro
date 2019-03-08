6974b8a8a2a064b035b6fa14c7ac713bf3b45fae
Rodrigo <Rodrigo>
2019-03-07 21:38:37 -0300

    Ninject

---
+++ Financas/Financas.Infrastructure/Repository/UsuarioRepository.cs
@@ -0,0 +1,26 @@
+﻿using Financas.Domain;
+using Financas.Infrastructure.Context;
+using System.Collections.Generic;
+
+namespace Financas.Infrastructure.Repository
+{
+    public class UsuarioRepository
+    {
+        private readonly FinancasContext context;
+
+        public UsuarioRepository(FinancasContext context)
+        {
+            this.context = context;
+        }
+
+        public void Adicionar(Usuario usario)
+        {
+
+        }
+
+        public IList<Usuario> Listar()
+        {
+            return null;
+        }
+    }
+}
