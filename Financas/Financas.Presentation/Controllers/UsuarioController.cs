a0c314cef97193adfdeea32675ac5629f7fbac4b
Rodrigo <Rodrigo>
2019-03-07 21:21:30 -0300

    Criando as ações

---
+++ Financas/Financas.Presentation/Controllers/UsuarioController.cs
@@ -0,0 +1,25 @@
+﻿using Financas.Presentation.Models;
+using System.Web.Mvc;
+
+namespace Financas.Presentation.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public ActionResult Form()
+        {
+            return View();
+        }
+
+        public ActionResult Cadastro(UsuarioViewModel usuarioView)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction(null);
+            }
+            else
+            {
+                return View("Form", usuarioView);
+            }
+        }
+    }
+}
---
+++ Financas/Financas.Presentation/Models/UsuarioViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Financas.Presentation.Models
+{
+    public class UsuarioViewModel
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+    }
+}
